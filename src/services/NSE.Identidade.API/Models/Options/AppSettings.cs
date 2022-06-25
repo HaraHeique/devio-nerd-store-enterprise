35ff62f6a42d2677fd9f8ff7919d259df435841a
HaraHeique <heikacademicos@hotmail.com>
2022-06-25 18:09:41 -0300

    feat: configuração do autenticação via JWT e separação lógica das Models para Options e ViewModels

---
+++ src/services/NSE.Identidade.API/Models/Options/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace NSE.Identidade.API.Models
+{
+    public class AppSettings
+    {
+        public const string SettingsKey = "AppSettings";
+
+        public string Secret { get; set; }
+        public int ExpiracaoHoras { get; set; }
+        public string Emissor { get; set; }
+        public string ValidoEm { get; set; }
+    }
+}
--- src/services/NSE.Identidade.API/Program.cs
+++ src/services/NSE.Identidade.API/Program.cs
@@ -1,10 +1,15 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using NSE.Identidade.API.Data;
+using NSE.Identidade.API.Models;
+using System.Text;
 
 namespace NSE.Identidade.API
 {
-    #pragma warning disable
+#pragma warning disable
     public class Program
     {
         private static WebApplicationBuilder _builder;
@@ -37,10 +42,51 @@ namespace NSE.Identidade.API
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
+            var appSettingsSection = configuration.GetSection(AppSettings.SettingsKey);
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = true;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(appSettings.Secret)),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = appSettings.ValidoEm,
+                    ValidIssuer = appSettings.Emissor
+                };
+            });
+
             services.AddControllers();
 
             services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "NerdStore Enterprise Identity API",
+                    Description = "Esta API faz parte do curso ASP.NET Core Enterprise Applications.",
+                    Contact = new OpenApiContact 
+                    {
+                        Name = "Harã Heique",
+                        Email = "heikacademicos@hotmail.com"
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "MIT",
+                        Url = new Uri("https://opensource.org/licenses/MIT")
+                    }
+                });
+            });
         }
 
         private static void ConfigureRequestsPipeline()
@@ -48,7 +94,10 @@ namespace NSE.Identidade.API
             if (_app.Environment.IsDevelopment())
             {
                 _app.UseSwagger();
-                _app.UseSwaggerUI();
+                _app.UseSwaggerUI(config =>
+                {
+                    config.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                });
             }
 
             _app.UseHttpsRedirection();
