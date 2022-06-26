46f0eac022221fafa4c732ef8a7fc91a653d514b
HaraHeique <heikacademicos@hotmail.com>
2022-06-26 12:06:00 -0300

    feat: implementação da geração do JWT com key simétrica

--- src/services/NSE.Identidade.API/Controllers/AuthController.cs
+++ src/services/NSE.Identidade.API/Controllers/AuthController.cs
@@ -1,22 +1,30 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using NSE.Identidade.API.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace NSE.Identidade.API.Controllers
 {
     [ApiController]
     [Route("api/identidade")]
-    public class AuthController : Controller
+    public class AuthController : ControllerBase
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly AppSettings _appSettings;
 
         public AuthController(
-            UserManager<IdentityUser> userManager, 
-            SignInManager<IdentityUser> signInManager)
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager, 
+            IOptions<AppSettings> appSettings)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _appSettings = appSettings.Value;
         }
 
         [HttpPost("nova-conta")]
@@ -36,7 +44,8 @@ namespace NSE.Identidade.API.Controllers
             if (result.Succeeded)
             {
                 await _signInManager.SignInAsync(usuario, false);
-                return Ok();
+                var respotaLoginVM = await GerarJwt(usuario.Email);
+                return Ok(respotaLoginVM);
             }
 
             return BadRequest();
@@ -51,10 +60,72 @@ namespace NSE.Identidade.API.Controllers
 
             if (result.Succeeded)
             {
-                return Ok("Login bem sucedido!");
+                var respotaLoginVM = await GerarJwt(usuarioLogin.Email);
+                return Ok(respotaLoginVM);
             }
 
             return BadRequest("Usuário inválido");
+        }
+
+        private async Task<UsuarioRespostaLoginViewModel> GerarJwt(string email)
+        {
+            // TODO Fazer uma refatoração depois colocando em um service com método independentes
+
+            // Parte que obtém as claims
+            var user = await _userManager.FindByEmailAsync(email);
+            var claims = await _userManager.GetClaimsAsync(user);
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow).ToString()));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64));
+            AdicionarRolesComoClaims(claims, userRoles);
+
+            // Parte que gera o token
+            var identityClaims = new ClaimsIdentity();
+            identityClaims.AddClaims(claims);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            byte[] key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+
+            var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = _appSettings.Emissor,
+                Audience = _appSettings.ValidoEm,
+                Subject = identityClaims,
+                Expires = DateTime.UtcNow.AddHours(_appSettings.ExpiracaoHoras),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            });
+
+            string encodedToken = tokenHandler.WriteToken(token);
+
+            // Parte que gera a respota do objeto retornado para o client com o token junto
+            return new UsuarioRespostaLoginViewModel
+            {
+                AccessToken = encodedToken,
+                ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalMilliseconds,
+                UsuarioToken = new UsuarioTokenViewModel
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Claims = claims.Select(c => new UsuarioClaimViewModel
+                    {
+                        Type = c.Type,
+                        Value = c.Value
+                    })
+                }
+            };
+
+            static long ToUnixEpochDate(DateTime date) 
+                => (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
+
+            static void AdicionarRolesComoClaims(IList<Claim> claims, IList<string> roles) 
+            {
+                foreach (string userRole in roles)
+                    claims.Add(new Claim(ClaimTypes.Role, userRole));
+            }
         }
     }
 }
