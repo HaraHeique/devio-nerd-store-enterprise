295340105ccb77355fb7fa02c26c8354c65d01f7
HaraHeique <heikacademicos@hotmail.com>
2022-08-13 17:55:41 -0300

    feat: implementação genérica do MessageBus usando EasyNetQ (adotando padrão DI)

---
+++ src/building-blocks/NSE.Infra/NSE.Infra.MessageBus/DependencyInjectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace NSE.Infra.MessageBus
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddMessageBus(this IServiceCollection services, string connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString)) throw new ArgumentNullException();
+
+            services.AddSingleton<IMessageBus>(new MessageBus(connectionString));
+
+            return services;
+        }
+    }
+}
---
+++ src/building-blocks/NSE.Infra/NSE.Infra.MessageBus/MessageBus.cs
@@ -0,0 +1,107 @@
+﻿using EasyNetQ;
+using EasyNetQ.Internals;
+using NSE.Core.Messages.Integration;
+using Polly;
+using RabbitMQ.Client.Exceptions;
+
+namespace NSE.Infra.MessageBus
+{
+    public class MessageBus : IMessageBus
+    {
+        private IBus _bus;
+        private IAdvancedBus _advancedBus;
+
+        private readonly string _connectionString;
+
+        public MessageBus(string connectionString)
+        {
+            _connectionString = connectionString;
+            TryConnect();
+        }
+
+        public bool IsConnected => _bus?.Advanced.IsConnected ?? false;
+        public IAdvancedBus AdvancedBus => _bus?.Advanced;
+
+        public void Publish<T>(T message) where T : IntegrationEvent
+        {
+            TryConnect();
+            _bus.PubSub.PublishAsync(message);
+        }
+
+        public async Task PublishAsync<T>(T message) where T : IntegrationEvent
+        {
+            TryConnect();
+            await _bus.PubSub.PublishAsync(message);
+        }
+
+        public void Subscribe<T>(string subscriptionId, Action<T> onMessage) where T : class
+        {
+            TryConnect();
+            _bus.PubSub.Subscribe(subscriptionId, onMessage);
+        }
+
+        public AwaitableDisposable<SubscriptionResult> SubscribeAsync<T>(string subscriptionId,
+            Func<T, Task> onMessage) where T : class
+        {
+            TryConnect();
+            return _bus.PubSub.SubscribeAsync(subscriptionId, onMessage);
+        }
+
+        public TResponse Request<TRequest, TResponse>(TRequest request) where TRequest : IntegrationEvent
+            where TResponse : ResponseMessage
+        {
+            TryConnect();
+            return _bus.Rpc.Request<TRequest, TResponse>(request);
+        }
+
+        public async Task<TResponse> RequestAsync<TRequest, TResponse>(TRequest request)
+            where TRequest : IntegrationEvent where TResponse : ResponseMessage
+        {
+            TryConnect();
+            return await _bus.Rpc.RequestAsync<TRequest, TResponse>(request);
+        }
+
+        public IDisposable Respond<TRequest, TResponse>(Func<TRequest, TResponse> responder)
+            where TRequest : IntegrationEvent where TResponse : ResponseMessage
+        {
+            TryConnect();
+            return _bus.Rpc.Respond(responder);
+        }
+
+        public AwaitableDisposable<IDisposable> RespondAsync<TRequest, TResponse>(
+            Func<TRequest, Task<TResponse>> responder)
+            where TRequest : IntegrationEvent where TResponse : ResponseMessage
+        {
+            TryConnect();
+            return _bus.Rpc.RespondAsync(responder);
+        }
+
+        private void TryConnect()
+        {
+            if (IsConnected) return;
+
+            var policy = Policy.Handle<EasyNetQException>()
+                .Or<BrokerUnreachableException>()
+                .RetryForever();
+
+            policy.Execute(() =>
+            {
+                _bus = RabbitHutch.CreateBus(_connectionString);
+                _advancedBus = _bus.Advanced;
+                _advancedBus.Disconnected += OnDisconnect;
+            });
+        }
+
+        // Método existente para caso haja queda no serviço de mensageria/filas, melhorando assim a resiliência
+        private void OnDisconnect(object s, EventArgs e)
+        {
+            var policy = Policy.Handle<EasyNetQException>()
+                .Or<BrokerUnreachableException>()
+                .RetryForever();
+
+            policy.Execute(TryConnect);
+        }
+
+        public void Dispose() => _bus.Dispose();
+    }
+}
