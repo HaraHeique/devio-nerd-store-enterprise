3a9267ff2a524d1eff40786e3fbf342017f606d9
HaraHeique <heikacademicos@hotmail.com>
2022-08-14 18:36:49 -0300

    feat: inicialização do projeto da API de carrinho

---
+++ src/services/NSE.Carrinho.API/Configurations/DependencyInjectionConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace NSE.Carrinho.API.Configurations
+{
+    public static class DependencyInjectionConfig
+    {
+        public static WebApplicationBuilder RegisterDependencies(this WebApplicationBuilder builder)
+        {
+            //builder.Services.AddScoped<CatalogoContext>();
+            //builder.Services.AddDbContext<CatalogoContext>(options =>
+            //    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+            //);
+
+            //builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+
+            return builder;
+        }
+    }
+}
---
+++ src/services/NSE.Carrinho.API/Controllers/CarrinhoController.cs
@@ -0,0 +1,8 @@
+﻿using NSE.WebAPI.Core.Controllers;
+
+namespace NSE.Carrinho.API.Controllers
+{
+    public class CarrinhoController : MainController
+    {
+    }
+}
